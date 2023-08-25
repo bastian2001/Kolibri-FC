f3d71a1c0722a834fb366ba7b5e47dab6f55ed36
Bastian Springer <bas.c.spr@gmail.com>
2023-08-26 01:22:19 +0200

    converter from rpbb to json done, incl. some examples

--- BB Decoder Test/test.cpp
+++ BB Decoder Test/test.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-ifstream file("./36.rpbb.txt");
+ifstream file;
+
 char readInt8()
 {
 	char reader;
@@ -94,24 +95,116 @@ Flag logFlags[] = {
 	{"LOG_ALTITUDE", 2},
 };
 
+string joinArray(string arr[], int len, string sep = ", ")
+{
+	string ret = "";
+	for (int i = 0; i < len; i++)
+	{
+		ret += arr[i];
+		if (i < len - 1)
+			ret += sep;
+	}
+	return ret;
+}
+
+bool hasEnding(std::string const &fullString, std::string const &ending)
+{
+	if (fullString.length() >= ending.length())
+	{
+		return (0 == fullString.compare(fullString.length() - ending.length(), ending.length(), ending));
+	}
+	else
+	{
+		return false;
+	}
+}
+int handleFile(string fname, bool print = true);
+
 int main()
 {
+	// list all .rpbb files in current directory
+	system("dir /b *.rpbb,*.txt > list.tmp");
+	ifstream list("list.tmp");
+	string files[100];
+	int fileCount = 0;
+	while (!list.eof())
+	{
+		getline(list, files[fileCount++]);
+		if (fileCount >= 100)
+			break;
+	}
+	fileCount--;
+	list.close();
+	system("del list.tmp");
+	// ask user which file to open using 2 digit number, or to enter a path
+	cout << "\nSelect file to open\nLeave empty to do all\nEnter relative path to do another file"
+		 << endl;
+	for (int i = 0; i < fileCount; i++)
+	{
+		printf("[%02d] %s\n", i, files[i].c_str());
+	}
+	cout << ">>  ";
+	string fname = "";
+	getline(cin, fname);
+	if (fname.length() == 2)
+	{
+		int i = (fname[0] - '0') * 10 + (fname[1] - '0');
+		if (i >= 0 && i < fileCount)
+		{
+			// check if entry was actually that number
+			char c[3];
+			snprintf(c, 3, "%02d", i);
+			if (fname.substr(0, 2) == c)
+			{
+				fname = files[i];
+			}
+		}
+	}
+	if (fname == "")
+	{
+		for (int i = 0; i < fileCount; i++)
+			handleFile(files[i], false);
+		return 0;
+	}
+	// open file
+	int ret = handleFile(fname);
+	if (ret != 0)
+		return ret;
+	return 0;
+}
+
+int handleFile(const string fname, bool print)
+{
+	file.open(fname);
+	if (!file.is_open())
+	{
+		cout << "Error opening file \"" << fname << "\", terminating\n";
+		return 2;
+	}
 	file.ignore(23);
 
 	uint32_t magic = readUInt32();
 	bool b = magic == 0x99A12720;
-	cout << (b ? "Magic bytes correct\n" : "Magic bytes incorrect, terminating\n");
 	if (!b)
 	{
+		cout << "Magic bytes incorrect in file " << fname << ", terminating\n";
 		printf("0x%08X", magic);
 		return 1;
 	}
-	cout << "File version: " << (int)readUInt8() << '.' << (int)readUInt8() << '.' << (int)readUInt8() << endl;
-	cout << "Start time: " << readUInt32() << endl;
-	cout << "PID loop frequency: " << (3200 / (1 << readUInt8())) << " Hz" << endl;
-	cout << "Loop divider: " << (int)readUInt8() << endl;
+	string fileVersion = to_string(readUInt8()) + '.' + to_string(readUInt8()) + '.' + to_string(readUInt8());
+	uint32_t startTime = readUInt32();
+	uint32_t pidLoopFreq = (3200 / (1 << readUInt8()));
+	uint32_t loopDivider = readUInt8();
+	uint32_t logFreq = pidLoopFreq / loopDivider;
 	uint8_t gyrAccelRange = readUInt8();
-	cout << "Gyro/Accel range: " << (gyrAccelRange & 0b111) << ' ' << (gyrAccelRange >> 3) << endl;
+	if (print)
+	{
+		cout << "File version: " << fileVersion << endl;
+		cout << "Start time: " << startTime << endl;
+		cout << "PID loop frequency: " << pidLoopFreq << " Hz" << endl;
+		cout << "Loop divider: " << loopDivider << endl;
+		cout << "Gyro/Accel range: " << (gyrAccelRange & 0b111) << ' ' << (gyrAccelRange >> 3) << endl;
+	}
 	double rateFactors[5][3];
 	for (int i = 0; i < 5; i++)
 	{
@@ -120,17 +213,20 @@ int main()
 			rateFactors[i][j] = (double)readInt32() / 65536.;
 		}
 	}
-	cout << "Rate factors:\tx^1\tx^2\tx^3\tx^4\tx^5" << endl;
-	cout << "\tRoll:\t";
-	for (int i = 0; i < 5; i++)
-		cout << rateFactors[i][0] << '\t';
-	cout << "\n\tPitch:\t";
-	for (int i = 0; i < 5; i++)
-		cout << rateFactors[i][1] << '\t';
-	cout << "\n\tYaw:\t";
-	for (int i = 0; i < 5; i++)
-		cout << rateFactors[i][2] << '\t';
-	cout << endl;
+	if (print)
+	{
+		cout << "Rate factors:\tx^1\tx^2\tx^3\tx^4\tx^5" << endl;
+		cout << "\tRoll:\t";
+		for (int i = 0; i < 5; i++)
+			cout << rateFactors[i][0] << '\t';
+		cout << "\n\tPitch:\t";
+		for (int i = 0; i < 5; i++)
+			cout << rateFactors[i][1] << '\t';
+		cout << "\n\tYaw:\t";
+		for (int i = 0; i < 5; i++)
+			cout << rateFactors[i][2] << '\t';
+		cout << endl;
+	}
 	double pidConstants[3][7];
 	for (int i = 0; i < 3; i++)
 	{
@@ -139,42 +235,48 @@ int main()
 			pidConstants[i][j] = (double)readInt32() / 65536.;
 		}
 	}
-	cout << "PID constants:\tkP\tkI\t\tkD\tkFF\tkS\t\tiFalloff\treserved" << endl;
-	cout << "\tRoll:\t";
-	for (int i = 0; i < 7; i++)
+	if (print)
 	{
-		cout << pidConstants[0][i] << '\t';
+		cout << "PID constants:\tkP\tkI\t\tkD\tkFF\tkS\t\tiFalloff\treserved" << endl;
+		cout << "\tRoll:\t";
+		for (int i = 0; i < 7; i++)
+		{
+			cout << pidConstants[0][i] << '\t';
+		}
+		cout << "\n\tPitch:\t";
+		for (int i = 0; i < 7; i++)
+		{
+			cout << pidConstants[1][i] << '\t';
+		}
+		cout << "\n\tYaw:\t";
+		for (int i = 0; i < 7; i++)
+		{
+			cout << pidConstants[2][i] << '\t';
+		}
+		cout << endl;
+		cout << "Enabled flags:" << endl;
 	}
-	cout << "\n\tPitch:\t";
-	for (int i = 0; i < 7; i++)
-	{
-		cout << pidConstants[1][i] << '\t';
-	}
-	cout << "\n\tYaw:\t";
-	for (int i = 0; i < 7; i++)
-	{
-		cout << pidConstants[2][i] << '\t';
-	}
-	cout << endl;
-	cout << "Enabled flags:" << endl;
 	uint64_t bbFlags = readUInt64();
 	uint32_t dataFrameLength = 0;
 	for (int i = 0; i < (sizeof(logFlags)) / (sizeof(Flag)); i++)
 	{
 		if (bbFlags & (1 << i))
 		{
-			cout << "  - " << logFlags[i].name << endl;
+			if (print)
+				cout << "  - " << logFlags[i].name << endl;
 			dataFrameLength += logFlags[i].length;
 		}
 	}
-	cout << "  => " << dataFrameLength << " Bytes per data frame" << endl;
+	if (print)
+		cout << "  => " << dataFrameLength << " Bytes per data frame" << endl;
 	int pos = file.tellg();
 	file.seekg(0, ios::end);
 	int end = file.tellg();
 	end -= 30;
 	end -= pos;
 	int frameCount = end / dataFrameLength;
-	cout << "  => " << frameCount << " data frames" << endl;
+	if (print)
+		cout << "  => " << frameCount << " data frames" << endl;
 	if (end % dataFrameLength != 0)
 		cout << "Warning: " << (end % dataFrameLength) << " Bytes of trailing data" << endl;
 	if (bbFlags & (1 << 0))
@@ -309,14 +411,398 @@ int main()
 			altitude[i] = readUInt16();
 	}
 
-	if (bbFlags & (1 << 26))
+	file.close();
+
+	string ofname = "./";
+	ofname += fname;
+	if (hasEnding(ofname, ".rpbb.txt"))
+		ofname = ofname.substr(0, ofname.length() - 9);
+	else if (hasEnding(ofname, ".rpbb"))
+		ofname = ofname.substr(0, ofname.length() - 5);
+	else if (hasEnding(ofname, ".txt"))
+		ofname = ofname.substr(0, ofname.length() - 4);
+	ofname += ".json";
+	ofstream json(ofname);
+	json << "{\n";
+	json << "\t\"version\": \"" << fileVersion << "\",\n";
+	json << "\t\"startTime\": " << startTime << ",\n";
+	json << "\t\"pidLoopFreq\": " << pidLoopFreq << ",\n";
+	json << "\t\"loopDivider\": " << loopDivider << ",\n";
+	json << "\t\"gyroRange\": " << (2000 >> (gyrAccelRange & 0b111)) << ",\n";
+	json << "\t\"accelRange\": " << (2 << (gyrAccelRange >> 3)) << ",\n";
+	json << "\t\"rateFactors\": {\n";
+	json << "\t\t\"roll\": [";
+	for (int i = 0; i < 5; i++)
 	{
-		cout << "Motor outputs:" << endl;
+		json << rateFactors[i][0];
+		if (i < 4)
+			json << ", ";
+	}
+	json << "],\n";
+	json << "\t\t\"pitch\": [";
+	for (int i = 0; i < 5; i++)
+	{
+		json << rateFactors[i][1];
+		if (i < 4)
+			json << ", ";
+	}
+	json << "],\n";
+	json << "\t\t\"yaw\": [";
+	for (int i = 0; i < 5; i++)
+	{
+		json << rateFactors[i][2];
+		if (i < 4)
+			json << ", ";
+	}
+	json << "]\n";
+	json << "\t},\n";
+	json << "\t\"pidConstants\": {\n";
+	json << "\t\t\"roll\": {\n";
+	json << "\t\t\t\"kP\": " << pidConstants[0][0] << ",\n";
+	json << "\t\t\t\"kI\": " << pidConstants[0][1] << ",\n";
+	json << "\t\t\t\"kD\": " << pidConstants[0][2] << ",\n";
+	json << "\t\t\t\"kFF\": " << pidConstants[0][3] << ",\n";
+	json << "\t\t\t\"kS\": " << pidConstants[0][4] << ",\n";
+	json << "\t\t\t\"iFalloff\": " << pidConstants[0][5] << "\n";
+	json << "\t\t},\n";
+	json << "\t\t\"pitch\": {\n";
+	json << "\t\t\t\"kP\": " << pidConstants[1][0] << ",\n";
+	json << "\t\t\t\"kI\": " << pidConstants[1][1] << ",\n";
+	json << "\t\t\t\"kD\": " << pidConstants[1][2] << ",\n";
+	json << "\t\t\t\"kFF\": " << pidConstants[1][3] << ",\n";
+	json << "\t\t\t\"kS\": " << pidConstants[1][4] << ",\n";
+	json << "\t\t\t\"iFalloff\": " << pidConstants[1][5] << "\n";
+	json << "\t\t},\n";
+	json << "\t\t\"yaw\": {\n";
+	json << "\t\t\t\"kP\": " << pidConstants[2][0] << ",\n";
+	json << "\t\t\t\"kI\": " << pidConstants[2][1] << ",\n";
+	json << "\t\t\t\"kD\": " << pidConstants[2][2] << ",\n";
+	json << "\t\t\t\"kFF\": " << pidConstants[2][3] << ",\n";
+	json << "\t\t\t\"kS\": " << pidConstants[2][4] << ",\n";
+	json << "\t\t\t\"iFalloff\": " << pidConstants[2][5] << "\n";
+	json << "\t\t}\n";
+	json << "\t},\n";
+	json << "\t\"enabledFlags\": [\n";
+	string flags[(sizeof(logFlags)) / (sizeof(Flag))];
+	int j = 0;
+	for (int i = 0; i < (sizeof(logFlags)) / (sizeof(Flag)); i++)
+		if (bbFlags & (1 << i))
+			flags[j++] = logFlags[i].name;
+	json << "\t\t\"" << joinArray(flags, j, "\",\n\t\t\"") << "\"\n\t],\n";
+	json << "\t\"data\": {\n";
+	if (bbFlags & (1 << 0))
+	{
+		json << "\t\t\"rollELRSRaw\": [";
 		for (int i = 0; i < frameCount; i++)
 		{
-			cout << ((motorOutputs[i] >> 36) & 0xFFF) << ' ' << ((motorOutputs[i] >> 24) & 0xFFF) << ' ' << ((motorOutputs[i] >> 12) & 0xFFF) << ' ' << (motorOutputs[i] & 0xFFF) << endl;
+			json << rollELRSRaw[i];
+			if (i < frameCount - 1)
+				json << ", ";
 		}
+		json << "\n\t\t],\n";
 	}
-
+	if (bbFlags & (1 << 1))
+	{
+		json << "\t\t\"pitchELRSRaw\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << pitchELRSRaw[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 2))
+	{
+		json << "\t\t\"throttleELRSRaw\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << throttleELRSRaw[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 3))
+	{
+		json << "\t\t\"yawELRSRaw\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << yawELRSRaw[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 4))
+	{
+		json << "\t\t\"rollSetpoint\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << rollSetpoint[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 5))
+	{
+		json << "\t\t\"pitchSetpoint\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << pitchSetpoint[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 6))
+	{
+		json << "\t\t\"throttleSetpoint\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << throttleSetpoint[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 7))
+	{
+		json << "\t\t\"yawSetpoint\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << yawSetpoint[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 8))
+	{
+		json << "\t\t\"rollGyroRaw\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << rollGyroRaw[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 9))
+	{
+		json << "\t\t\"pitchGyroRaw\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << pitchGyroRaw[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 10))
+	{
+		json << "\t\t\"yawGyroRaw\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << yawGyroRaw[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 11))
+	{
+		json << "\t\t\"rollPIDP\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << rollPIDP[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 12))
+	{
+		json << "\t\t\"rollPIDI\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << rollPIDI[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 13))
+	{
+		json << "\t\t\"rollPIDD\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << rollPIDD[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 14))
+	{
+		json << "\t\t\"rollPIDFF\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << rollPIDFF[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 15))
+	{
+		json << "\t\t\"rollPIDS\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << rollPIDS[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 16))
+	{
+		json << "\t\t\"pitchPIDP\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << pitchPIDP[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 17))
+	{
+		json << "\t\t\"pitchPIDI\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << pitchPIDI[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 18))
+	{
+		json << "\t\t\"pitchPIDD\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << pitchPIDD[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 19))
+	{
+		json << "\t\t\"pitchPIDFF\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << pitchPIDFF[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 20))
+	{
+		json << "\t\t\"pitchPIDS\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << pitchPIDS[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 21))
+	{
+		json << "\t\t\"yawPIDP\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << yawPIDP[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 22))
+	{
+		json << "\t\t\"yawPIDI\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << yawPIDI[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 23))
+	{
+		json << "\t\t\"yawPIDD\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << yawPIDD[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 24))
+	{
+		json << "\t\t\"yawPIDFF\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << yawPIDFF[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 25))
+	{
+		json << "\t\t\"yawPIDS\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << yawPIDS[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 26))
+	{
+		json << "\t\t\"motorOutputs\": [\n\t\t\t";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << '[' << ((motorOutputs[i] >> 36) & 0xFFF) << ", " << ((motorOutputs[i] >> 24) & 0xFFF) << ", " << ((motorOutputs[i] >> 12) & 0xFFF) << ", " << (motorOutputs[i] & 0xFFF) << ']';
+			if (i < frameCount - 1)
+				json << ",\n\t\t\t";
+		}
+		json << "\n\t\t],\n";
+	}
+	if (bbFlags & (1 << 27))
+	{
+		json << "\t\t\"altitude\": [";
+		for (int i = 0; i < frameCount; i++)
+		{
+			json << altitude[i];
+			if (i < frameCount - 1)
+				json << ", ";
+		}
+		json << "\n\t\t],\n";
+	}
+	json << "\t}\n";
+	json << "}\n";
+	json.close();
+	if (print)
+		cout << "\n\n";
+	cout << "Successfully converted " << fname << " to " << ofname.substr(2) << endl;
 	return 0;
 }
