ae3f8e01b613821d18ef9e4087631a8c5d61a710
bastian2001 <bas.c.spr@gmail.com>
2023-09-03 21:29:32 +0200

    current sensor (not working)

--- Firmware/src/adc.cpp
+++ Firmware/src/adc.cpp
@@ -1,6 +1,6 @@
 #include "global.h"
 
-float adcVoltage = 0, pVoltage = 0;
+float adcVoltage = 0, pVoltage = 0, adcCurrent = 0;
 elapsedMillis adcTimer = 0;
 
 void initADC()
@@ -8,25 +8,50 @@ void initADC()
 	adc_gpio_init(PIN_ADC_VOLTAGE);
 	adc_gpio_init(PIN_ADC_CURRENT);
 	adc_init();
-	adc_select_input(PIN_ADC_VOLTAGE - 26);
+	enableElem(OSDElem::TOT_VOLTAGE);
+	placeElem(OSDElem::TOT_VOLTAGE, 1, 1);
+	enableElem(OSDElem::CURRENT);
+	placeElem(OSDElem::CURRENT, 1, 2);
 }
+
+uint8_t adcType = 0; // 1 = voltage, 0 = current
 
 void adcLoop()
 {
 	if (adcTimer > 50)
 	{
 		adcTimer = 0;
-		uint16_t raw = adc_read();
-		adcVoltage = (raw - 1930) * (10.f) / (3041 - 1930) + 10;
-		adcVoltage = constrain(adcVoltage, 0, 30);
-		if (pVoltage > 14 && adcVoltage <= 14)
+		if (adcType)
 		{
-			makeSound(5000, 65535, 300, 300);
+			adc_select_input(PIN_ADC_VOLTAGE - 26);
+			uint16_t raw = adc_read();
+			adcVoltage = raw * (3.3f * 11.f / 4096.f);
+			if (pVoltage > 14 && adcVoltage <= 14)
+			{
+				makeSound(5000, 65535, 300, 300);
+			}
+			else if (adcVoltage > 14 && pVoltage <= 14)
+			{
+				stopSound();
+			}
+			uint8_t voltageStr[16] = {0};
+			uint8_t len = snprintf((char *)voltageStr, 16, "%.2f", adcVoltage);
+			voltageStr[len] = 0x06;
+			voltageStr[len + 1] = '\0';
+			updateElem(OSDElem::TOT_VOLTAGE, (char *)voltageStr);
+			pVoltage = adcVoltage;
 		}
-		else if (adcVoltage > 14 && pVoltage <= 14)
+		else
 		{
-			stopSound();
+			adc_select_input(PIN_ADC_CURRENT - 26);
+			uint16_t raw = adc_read();
+			adcCurrent = raw * (300.f / 4096.f); // 300A full deflection
+			uint8_t currentStr[16] = {0};
+			uint8_t len = snprintf((char *)currentStr, 16, "%.2f", adcCurrent);
+			currentStr[len] = 0x9A;
+			currentStr[len + 1] = '\0';
+			updateElem(OSDElem::CURRENT, (char *)currentStr);
 		}
-		pVoltage = adcVoltage;
+		adcType = !adcType;
 	}
 }
