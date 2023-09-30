84d498f01edd6aec3a4238a70aaf6df08665e0a5
bastian2001 <bas.c.spr@gmail.com>
2023-09-30 23:15:39 +0200

    auto fit

--- configurator/src/routes/tuning/+page.svelte
+++ configurator/src/routes/tuning/+page.svelte
@@ -158,7 +158,7 @@
 <style>
 	.wrapper {
 		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+		grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
 		margin: 1.5rem;
 		gap: 0rem 3rem;
 	}
