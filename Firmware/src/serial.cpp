892e22bbe311a2e267e2a81b1702aa194bd1e2ec
bastian2001 <bas.c.spr@gmail.com>
2024-02-09 02:39:47 +0100

    remove unnecessary modulo operation

--- Firmware/src/serial.cpp
+++ Firmware/src/serial.cpp
@@ -16,35 +16,33 @@ void serialLoop() {
 	elapsedMicros taskTimer = 0;
 	tasks[TASK_SERIAL].runCounter++;
 	for (int i = 0; i < 3; i++) {
-		if (serialFunctions[i % 3] & SERIAL_DISABLED)
+		if (serialFunctions[i] & SERIAL_DISABLED)
 			continue;
-		Stream *serial = serials[i % 3];
+		Stream *serial = serials[i];
 		int available  = serial->available();
-		if (available <= 0)
-			continue;
 		for (int j = 0; j < available; j++) {
 			readChar = serial->read();
-			if (serialFunctions[i % 3] & SERIAL_CONFIGURATOR) {
+			if (serialFunctions[i] & SERIAL_CONFIGURATOR) {
 				rp2040.wdt_reset();
 				elapsedMicros timer = 0;
 				configuratorHandleByte(readChar, i % 3);
 				taskTimer -= timer;
 			}
-			if (serialFunctions[i % 3] & SERIAL_ELRS) {
+			if (serialFunctions[i] & SERIAL_ELRS) {
 				if (elrsBuffer.itemCount() < 260)
 					elrsBuffer.push(readChar);
 			}
-			if (serialFunctions[i % 3] & SERIAL_ESC_TELEM) {
+			if (serialFunctions[i] & SERIAL_ESC_TELEM) {
 			}
-			if (serialFunctions[i % 3] & SERIAL_GPS) {
+			if (serialFunctions[i] & SERIAL_GPS) {
 				if (gpsBuffer.itemCount() < GPS_BUF_LEN)
 					gpsBuffer.push(readChar);
 			}
-			if (serialFunctions[i % 3] & SERIAL_IRC_TRAMP) {
+			if (serialFunctions[i] & SERIAL_IRC_TRAMP) {
 			}
-			if (serialFunctions[i % 3] & SERIAL_MSP) {
+			if (serialFunctions[i] & SERIAL_MSP) {
 			}
-			if (serialFunctions[i % 3] & SERIAL_SMARTAUDIO) {
+			if (serialFunctions[i] & SERIAL_SMARTAUDIO) {
 			}
 		}
 	}
