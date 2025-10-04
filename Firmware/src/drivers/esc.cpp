7f934e564dcecc6b53ee332ed8de4d6327bea1a2
Bastian <bas.c.spr@gmail.com>
2025-10-04 13:18:09 +0200

    revert non-bidir

--- Firmware/src/drivers/esc.cpp
+++ Firmware/src/drivers/esc.cpp
@@ -12,7 +12,7 @@ u32 escErpmFailCounter = 0;
 u16 dshotBeepTone = 1;
 static u8 motorPins[4] = {0};
 
-DShotX4 *escs[4];
+BidirDShotX1 *escs[4];
 
 void initMotorPins() {
 	for (int i = 0; i < 4; i++) {
@@ -65,70 +65,66 @@ void getMotorPins(u8 pins[4]) {
 void sendRaw11Bit(const u16 raw[4]) {
 	if (!enableDShot) return;
 	for (int i = 0; i < 4; i++) {
-		u16 r[4] = {raw[i], 0, 0, 0};
-		escs[i]->sendRaw11Bit(r);
+		escs[i]->sendRaw11Bit(raw[i]);
 	}
 }
 
 void sendThrottles(const i16 throttles[4]) {
 	if (!enableDShot) return;
-	u16 temp[4] = {0, 0, 0, 0};
 	for (int i = 0; i < 4; i++) {
 		i16 t = throttles[i];
 		if (t < 0) t = 0;
-		temp[0] = t;
-		escs[i]->sendThrottles(temp);
+		escs[i]->sendThrottle(t);
 	}
 }
 
 void decodeErpm() {
 	if (!enableDShot) return;
-	// TASK_START(TASK_ESC_RPM);
-	// escErpmFail = 0;
-	// for (int m = 0; m < 4; m++) {
-	// 	BidirDshotTelemetryType telemType = escs[m]->getTelemetryRaw((uint32_t *)&escRawTelemetry[m]);
-	// 	u32 telemVal = BidirDShotX1::convertFromRaw(escRawTelemetry[m], telemType);
-	// 	switch (telemType) {
-	// 	case BidirDshotTelemetryType::ERPM:
-	// 		if (escRpm[m] >= 0)
-	// 			escRpm[m] = telemVal / (MOTOR_POLES / 2);
-	// 		else
-	// 			escErpmFail |= 1 << m;
-	// 		break;
-	// 	case BidirDshotTelemetryType::TEMPERATURE:
-	// 		escTemp[m] = telemVal;
-	// 		break;
-	// 	case BidirDshotTelemetryType::VOLTAGE:
-	// 		escVoltage[m] = fix32(telemVal) >> 2;
-	// 		break;
-	// 	case BidirDshotTelemetryType::CURRENT:
-	// 		escCurrent[m] = telemVal;
-	// 		break;
-	// 	case BidirDshotTelemetryType::NO_PACKET:
-	// 		tasks[TASK_ESC_RPM].errorCount++;
-	// 		tasks[TASK_ESC_RPM].lastError = 1;
-	// 		escErpmFail |= 1 << m;
-	// 		escRawTelemetry[m] = 0;
-	// 		break;
-	// 	case BidirDshotTelemetryType::CHECKSUM_ERROR:
-	// 		tasks[TASK_ESC_RPM].errorCount++;
-	// 		tasks[TASK_ESC_RPM].lastError = 2;
-	// 		escErpmFail |= 1 << m;
-	// 		escRawTelemetry[m] = 0;
-	// 		break;
-	// 	case BidirDshotTelemetryType::STATUS:
-	// 	case BidirDshotTelemetryType::STRESS:
-	// 	case BidirDshotTelemetryType::DEBUG_FRAME_1:
-	// 	case BidirDshotTelemetryType::DEBUG_FRAME_2:
-	// 		escRawTelemetry[m] = 0;
-	// 	default:
-	// 		break;
-	// 	}
-	// }
-	escErpmFail = 0xF;
+	TASK_START(TASK_ESC_RPM);
+	escErpmFail = 0;
+	for (int m = 0; m < 4; m++) {
+		BidirDshotTelemetryType telemType = escs[m]->getTelemetryRaw((uint32_t *)&escRawTelemetry[m]);
+		u32 telemVal = BidirDShotX1::convertFromRaw(escRawTelemetry[m], telemType);
+		switch (telemType) {
+		case BidirDshotTelemetryType::ERPM:
+			if (escRpm[m] >= 0)
+				escRpm[m] = telemVal / (MOTOR_POLES / 2);
+			else
+				escErpmFail |= 1 << m;
+			break;
+		case BidirDshotTelemetryType::TEMPERATURE:
+			escTemp[m] = telemVal;
+			break;
+		case BidirDshotTelemetryType::VOLTAGE:
+			escVoltage[m] = fix32(telemVal) >> 2;
+			break;
+		case BidirDshotTelemetryType::CURRENT:
+			escCurrent[m] = telemVal;
+			break;
+		case BidirDshotTelemetryType::NO_PACKET:
+			tasks[TASK_ESC_RPM].errorCount++;
+			tasks[TASK_ESC_RPM].lastError = 1;
+			escErpmFail |= 1 << m;
+			escRawTelemetry[m] = 0;
+			break;
+		case BidirDshotTelemetryType::CHECKSUM_ERROR:
+			tasks[TASK_ESC_RPM].errorCount++;
+			tasks[TASK_ESC_RPM].lastError = 2;
+			escErpmFail |= 1 << m;
+			escRawTelemetry[m] = 0;
+			break;
+		case BidirDshotTelemetryType::STATUS:
+		case BidirDshotTelemetryType::STRESS:
+		case BidirDshotTelemetryType::DEBUG_FRAME_1:
+		case BidirDshotTelemetryType::DEBUG_FRAME_2:
+			escRawTelemetry[m] = 0;
+		default:
+			break;
+		}
+	}
 	if (escErpmFail)
 		escErpmFailCounter++;
 	else
 		escErpmFailCounter = 0;
-	// TASK_END(TASK_ESC_RPM);
+	TASK_END(TASK_ESC_RPM);
 }
