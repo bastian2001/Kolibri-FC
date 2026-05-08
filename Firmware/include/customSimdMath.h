8868f52dba306e67462e03bdc839f34b7138f7cf
Bastian <bas.c.spr@gmail.com>
2026-05-09 01:28:15 +0200

    add license to firmware

--- Firmware/include/customSimdMath.h
+++ Firmware/include/customSimdMath.h
@@ -1,3 +1,25 @@
+/**
+ * @file customSimdMath.h
+ * @brief Functions to speed up some math operations using ARM SIMD instructions. Only used on Cortex-M4 and M7 targets.
+ *
+ * Copyright (c) 2026 Kolibri-FC contributors
+ *
+ * This file is part of Kolibri-FC (https://github.com/bastian2001/Kolibri-FC).
+ *
+ * Kolibri-FC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kolibri-FC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kolibri-FC. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "arm_acle.h"
 
 #if __ARM_FEATURE_SIMD32
