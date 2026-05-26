bf380df5beb9ba73644737d7cd510999c2b3075b
Bastian <bas.c.spr@gmail.com>
2026-05-27 01:08:56 +0200

    Update osdCanvas.cpp

--- Firmware/src/osd/osdCanvas.cpp
+++ Firmware/src/osd/osdCanvas.cpp
@@ -559,7 +559,7 @@ void OsdCanvas::drawElement(u32 index) {
 		copyFrameBuffer(frameBuffer, buf, width, height, element.col, element.row, 2);
 	} break;
 	case OsdElementType::DEBUG_3: {
-		printOnBuffer(element, "HW:%d SW:%d", magHeading, softMagHeading);
+		printOnBuffer(element, "HW:%d SW:%d", (hardMagHeading * FIX_RAD_TO_DEG).geti32(), (softMagHeading * FIX_RAD_TO_DEG).geti32());
 	} break;
 	case OsdElementType::DEBUG_4: {
 	} break;
